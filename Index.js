af75569bbf1010908f6bcd4783c2144dad6a9188
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-06-21 19:10:41 +0500

    App Ready , Responsive and All Functions Working

--- Index.js
+++ Index.js
@@ -27,42 +27,72 @@ const DayOFWeek=(Index)=>
     return Arr[Index-1];
 };
 
-let Weather=()=>
+let Weather =()=>
 {
-    fetch('http://api.weatherapi.com/v1/forecast.json?key=e3e5a6e2101943eb83550323222106&q=lahore&aqi=no' , {method:'GET' , headers:{Accept:'application.json'}})
-    .then((Response)=>
+    document.querySelector('.Btn').addEventListener('click' , Caller=async()=>
     {
-        return Response.json();
-    })
-    .then((Data)=>
-    {
-        Loc.innerHTML=Data.location.name;
-        Con.innerHTML=Data.location.country;
-        Temp.innerHTML=Data.current.temp_c + ' *C';
-        Status.innerHTML=Data.current.condition.text;
-        Status.style.color="black";
-        Img.setAttribute('src' , Data.current.condition.icon);
+        let Query=document.querySelector('input').value;
+        if(Query.length==0)
+        {
+            Query='Lahore';
+        }
+        document.querySelector('.Hourly-Data').style.display='grid';
+        let Link=`http://api.weatherapi.com/v1/forecast.json?key=e3e5a6e2101943eb83550323222106&q=${Query}&aqi=no`;
 
-        Humi.innerHTML=Data.current.humidity+' %';
-        Rain.innerHTML=Data.current.cloud+' %';
-        Press.innerHTML=Data.current.pressure_in;
-        Wind.innerHTML=Data.current.wind_kph+' KM/H';
-        let Dat=Data.location.localtime;
+        if(Query.length!=0)
+        {
+            fetch(Link , {method:'GET' , headers:{Accept:'application.json'}})
+            .then((Response)=>
+            {
+                return Response.json();
+            })
+            .then((Data)=>
+            {
+                document.querySelector('.Left').style.visibility='visible';
+                document.querySelector('.Right').style.visibility='visible';
+                document.querySelector('.Day').style.visibility='visible';
+                if(Data.current.is_day!=1)
+                {
+                    document.querySelector('.Main').style.backgroundImage="url('/Images/Night.jpg')"
+                }
+                Loc.innerHTML=Data.location.name;
+                Con.innerHTML=Data.location.country;
+                Temp.innerHTML=Data.current.temp_c + ' *C';
+                Status.innerHTML=Data.current.condition.text;
+                Status.style.color="black";
+                Img.setAttribute('src' , Data.current.condition.icon);
 
-        let Year=Dat.substr(0,4);
-        let Month=Dat.substr(5,2);
-        let Din=Dat.substr(8,2);
-        let Hr=Dat.substr(11,2);
-        let Min=Dat.substr(14,2);
-        console.log(Hr , Min);
+                Humi.innerHTML=Data.current.humidity+' %';
+                Rain.innerHTML=Data.current.cloud+' %';
+                Press.innerHTML=Data.current.pressure_in;
+                Wind.innerHTML=Data.current.wind_kph+' KM/H';
+                let Dat=Data.location.localtime;
 
-        var CurrDat=new Date(Din , Month , Year);
-        Day.innerHTML=DayOFWeek(CurrDat.getDay());
-    })
-    .catch((err)=>
-    {
-        console.log("ERROR OCCURED");
-    })
+                let Year=Dat.substr(0,4);
+                let Month=Dat.substr(5,2);
+                let Din=Dat.substr(8,2);
+                let Hr=Dat.substr(11,2);
+                let Min=Dat.substr(14,2);
+
+                var CurrDat=new Date(Din , Month , Year);
+                Day.innerHTML=DayOFWeek(CurrDat.getDay());
+
+                let Arr=document.querySelectorAll('.Hours .Time');
+                let Arr2=document.querySelectorAll('.Hours .Temp');
+                for (let i=0 ; i<7 ; i++)
+                {
+                    Arr[i].innerHTML=Data.forecast.forecastday[0].hour[(i+(+Hr))%12+1].time.substr(11,2)+':00';
+                    Arr2[i].innerHTML=Data.forecast.forecastday[0].hour[(i+(+Hr))%12+1].temp_c+' *C';
+                    Arr2[i].style.color='white';
+                }
+            })
+            .catch((err) =>
+            {
+                alert("This City/Contry Not Found , Please Try Other Keywords 💔💔")
+            });
+        }
+    });
 };
+
 
 Weather();
